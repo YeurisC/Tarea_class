132b6fbb54ee5452344d2176dac245d9b028414f
Yeuris <yeurisciberioestrella@gmail.com>
2025-04-06 14:07:36 -0400

    archivo para poder hacer tarea

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+'''
+    Calculadoras hecha en python
+'''
+resp = 's'
+while resp == 's':
+    #TODO:
+    #sumar, restar, multiplicar y dividir
+
+    numero1 = int(input('Ingrese el valor 1: '))
+    numero2 = int(input('ingrese valor 2: '))
+    suma = numero1 + numero2
+    resta = numero1 - numero2
+    division = numero1 / numero2
+
+    #suma
+    print(f"la suma es:{suma}")
+    print(f"la resta es:{resta}")
+    print(f"la division es:{division}")
+
+    resp = input('Desea volver a ejecutar(S/N): ')
