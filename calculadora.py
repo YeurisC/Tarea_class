9bc9c105d97cc86dd6af8d6e0a23594df12fbac0
Yeuris <yeurisciberioestrella@gmail.com>
2025-04-06 14:11:22 -0400

    variable para multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -11,10 +11,12 @@ while resp == 's':
     suma = numero1 + numero2
     resta = numero1 - numero2
     division = numero1 / numero2
+    multiplicacion = numero1 * numero2
 
     #suma
     print(f"la suma es:{suma}")
     print(f"la resta es:{resta}")
     print(f"la division es:{division}")
+    print(f"la multiplicacion es:{multiplicacion}")
 
     resp = input('Desea volver a ejecutar(S/N): ')
