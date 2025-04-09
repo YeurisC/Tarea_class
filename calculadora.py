c2a767cdf7f0c07d887bb49080c134cbcdb42075
Bryant <martinezperezbryant8@gmail.com>
2025-04-08 22:39:28 -0400

    Añadí comentarios explicativos y modifiqué el mensaje de bienvenida.

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,14 @@
 '''
     Calculadoras hecha en python
 '''
+#aloja el nombre del usuario
+nombre = input("¿Cual es tu nombre? ")
+
 resp = 's'
 while resp == 's':
     #TODO:
     #sumar, restar, multiplicar y dividir
-    print("Bienvenido a tu calculadora personal.")
+    print("Bienvenido a tu calculadora personal", nombre)
 
     numero1 = int(input('Ingrese el valor 1: '))
     numero2 = int(input('ingrese valor 2: '))
@@ -17,5 +20,5 @@ while resp == 's':
     print(f"la suma es:{suma}")
     print(f"la resta es:{resta}")
     print(f"la division es:{division}")
-
+    #pregunta para proxima ejecución
     resp = input('Desea volver a ejecutar(S/N): ')
